2abc1587298abe30c4204bce209b937a28059c4a
Kateria Wynn <iamkateria@gmail.com>
2022-07-25 13:01:42 -0700

    added rest of test funcs

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,19 +1,42 @@
 describe('calculateMonthlyPayment tests', function () {
-  const values = { amount: 30000, years: 8, rate: 8 };
+  beforeEach(function () {
+    values = { amount: 30000, years: 8, rate: 8 };
+  });
 
   afterEach(function () {
-    const values = {};
+    values = {};
   });
 
   it('should calculate the periodicInterestRate correctly', function () {
-    expect(calculateMonthlyPayment(values)).toEqual(199.47);
+    expect(
+      calculateMonthlyPayment({ amount: 30000, years: 8, rate: 14 })
+    ).toEqual('349.67');
   });
-  it('should calculate the principleAmount correctly', function () {});
-  it('should calculate totalPayments correctly', function () {});
+  it('should calculate the principleAmount correctly', function () {
+    expect(
+      calculateMonthlyPayment({ amount: 100000, years: 8, rate: 8 })
+    ).toEqual('666.14');
+  });
+  it('should calculate totalPayments correctly', function () {
+    expect(
+      calculateMonthlyPayment({ amount: 100000, years: 5, rate: 8 })
+    ).toEqual('666.00');
+  });
+  it('should calculate the monthlyPayment correctly', function () {
+    expect(calculateMonthlyPayment(values)).toEqual('199.47');
+  });
   it('should return a result with 2 decimal places', function () {
-    // ..
+    expect(calculateMonthlyPayment(values).indexOf('.')).toEqual(3);
   });
-  it('should return the monthlyPayment as a string', function () {});
-  it('should calculate the totalPayments correctly', function () {});
-  it('should throw an error when values are missing', function () {});
+  it('should return the monthlyPayment as a string', function () {
+    expect(typeof calculateMonthlyPayment(values)).toEqual('string');
+  });
+  it('should throw an error when values are missing', function () {
+    expect(() => calculateMonthlyPayment()).toThrowError();
+  });
+  it('should return 0 when loan amount is 0', function () {
+    expect(
+      calculateMonthlyPayment((values = { amount: 0, years: 8, rate: 8 }))
+    ).toEqual('0');
+  });
 });
