4c1301657bdf1a701a72f90aedbc85364d7a4c41
Kateria Wynn <iamkateria@gmail.com>
2022-07-25 15:36:51 -0700

    added test for submitServerInfo

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -10,10 +10,20 @@ describe('Servers test (with setup and tear-down)', function () {
     expect(Object.keys(allServers).length).toEqual(1);
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
+  it('should not add a new server to allServers if the serverName is empty', function () {
+    serverNameInput.value = '';
+
+    submitServerInfo();
+
+    expect(Object.keys(allServers).length).toEqual(0);
+  });
 
   afterEach(function () {
+    for (let server in allServers) {
+      let serverNum = server.slice(-1);
+      let currServer = document.querySelector(`#server${serverNum}`);
+      currServer.parentElement.replaceChildren();
+    }
     allServers = {};
-    let currServer = document.querySelector('#server1');
-    currServer.parentElement.replaceChildren();
   });
 });
