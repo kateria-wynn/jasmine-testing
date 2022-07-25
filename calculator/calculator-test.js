78acd2037850708a61119f913ae0e73a19045bfa
Kateria Wynn <iamkateria@gmail.com>
2022-07-24 21:48:23 -0700

    added beforeAll, describe, two additonal tests

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,17 @@
+// Goal: write tests for the calculateMonthlyPayment function
 
-it('should calculate the monthly rate correctly', function () {
-  // ...
+beforeAll(function () {
+  // create a new obj with sample values
 });
 
+describe('calculateMonthlyPayment tests', function () {
+  it('should calculate the periodicInterestRate correctly', function () {
+    // expect(calculateMonthlyPayment());
+  });
 
-it("should return a result with 2 decimal places", function() {
-  // ..
+  it('should return a result with 2 decimal places', function () {
+    // ..
+  });
+  it('should return the monthlyPayment as a string', function () {});
+  it('should calculate the totalPayments correctly', function () {});
 });
-
-/// etc
