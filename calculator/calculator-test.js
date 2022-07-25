fc49a9278af399296c6515a2d5112a06bbd236de
Kateria Wynn <iamkateria@gmail.com>
2022-07-25 11:47:36 -0700

    added all test funcs

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,17 +1,19 @@
-// Goal: write tests for the calculateMonthlyPayment function
-
-beforeAll(function () {
-  // create a new obj with sample values
-});
-
 describe('calculateMonthlyPayment tests', function () {
-  it('should calculate the periodicInterestRate correctly', function () {
-    // expect(calculateMonthlyPayment());
+  const values = { amount: 30000, years: 8, rate: 8 };
+
+  afterEach(function () {
+    const values = {};
   });
 
+  it('should calculate the periodicInterestRate correctly', function () {
+    expect(calculateMonthlyPayment(values)).toEqual(199.47);
+  });
+  it('should calculate the principleAmount correctly', function () {});
+  it('should calculate totalPayments correctly', function () {});
   it('should return a result with 2 decimal places', function () {
     // ..
   });
   it('should return the monthlyPayment as a string', function () {});
   it('should calculate the totalPayments correctly', function () {});
+  it('should throw an error when values are missing', function () {});
 });
