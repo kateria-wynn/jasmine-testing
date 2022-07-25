eb886f0b63363cb608e8917de7158470ee03cd84
Kateria Wynn <iamkateria@gmail.com>
2022-07-25 15:47:41 -0700

    added describe for submitServerInfo tests, test funcs for updateServerTable

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -3,19 +3,32 @@ describe('Servers test (with setup and tear-down)', function () {
     // initialization logic
     serverNameInput.value = 'Alice';
   });
+  describe('submitServerInfo tests', function () {
+    it('should add a new server to allServers on submitServerInfo()', function () {
+      submitServerInfo();
 
-  it('should add a new server to allServers on submitServerInfo()', function () {
-    submitServerInfo();
+      expect(Object.keys(allServers).length).toEqual(1);
+      expect(allServers['server' + serverId].serverName).toEqual('Alice');
+    });
+    it('should not add a new server to allServers if the serverName is empty', function () {
+      serverNameInput.value = '';
 
-    expect(Object.keys(allServers).length).toEqual(1);
-    expect(allServers['server' + serverId].serverName).toEqual('Alice');
-  });
-  it('should not add a new server to allServers if the serverName is empty', function () {
-    serverNameInput.value = '';
+      submitServerInfo();
 
-    submitServerInfo();
+      expect(Object.keys(allServers).length).toEqual(0);
+    });
 
-    expect(Object.keys(allServers).length).toEqual(0);
+    describe('updateServerTable tests', function () {
+      it('should not create a new table row if allServers is empty', function () {
+        const serverTable = document.querySelector('#serverTable');
+        allServers = {};
+        submitServerInfo();
+
+        expect(Object.keys(allServers).length === 0);
+        // expect server table to not have children
+        expect(serverTable.hasChildNodes()).toBe(true);
+      });
+    });
   });
 
   afterEach(function () {
