7b364ff46e553d61910d665052ee94e130effec4
Kateria Wynn <iamkateria@gmail.com>
2022-07-25 17:23:36 -0700

    added funcs for submitPaymentInfo tests

--- tip-pool/payments.test.js
+++ tip-pool/payments.test.js
@@ -4,6 +4,27 @@ describe('Payments tests', function () {
     tipAmtInput.value = '20';
   });
 
+  describe('submitPaymentInfo tests', function () {
+    it('should add a new payment to allPayments on submitServerInfo()', function () {
+      submitPaymentInfo();
+
+      expect(Object.keys(allPayments).length).toEqual(1);
+      expect(allPayments[`payment${paymentId}`].billAmt).toBe('100');
+      expect(allPayments[`payment${paymentId}`].tipAmt).toBe('20');
+    });
+    it('should not add a new payment to allPayments if the billAmt or tipAmt is empty', function () {
+      billAmtInput.value = '';
+      tipAmtInput.value = '';
+
+      submitPaymentInfo();
+
+      expect(Object.keys(allPayments).length).toEqual(0);
+    });
+  });
+  //   describe('createCurPayment tests', function () {});
+  //   describe('appendPaymentTable tests', function () {});
+  //   describe('updateSummary tests', function () {});
+
   afterEach(function () {
     for (let payment in allPayments) {
       let paymentNum = payment.slice(-1);
