a52072cb26c3aed6c87b77a21c61755070a88917
Kateria Wynn <iamkateria@gmail.com>
2022-07-27 13:17:13 -0700

    updated Helpers tests

--- tip-pool/helpers.test.js
+++ tip-pool/helpers.test.js
@@ -8,6 +8,8 @@ describe('Helpers tests', function () {
     it('should accept a bill amount', function () {
       submitPaymentInfo();
 
+      billAmt = allPayments['payment1'].billAmt;
+
       expect(sumPaymentTotal('billAmt')).toEqual(100);
     });
     it('should accept a tip amount', function () {
@@ -16,14 +18,27 @@ describe('Helpers tests', function () {
       expect(sumPaymentTotal('tipAmt')).toEqual(20);
     });
   });
+
   describe('calculateTipPercent tests', function () {
     it('should calculate tip percentage correctly', function () {
-      allPayments = {
-        payment1: { billAmt: '100', tipAmt: '20', tipPercent: 20 },
-      };
+      submitPaymentInfo();
+      createCurPayment();
+
       billAmt = allPayments['payment1'].billAmt;
       tipAmt = allPayments['payment1'].tipAmt;
+
       expect(calculateTipPercent(billAmt, tipAmt)).toEqual(20);
+    });
+  });
+
+  describe('appendTd tests', function () {
+    it('should upate the HTML with the new bill amount', function () {
+      tr = document.createElement('tr');
+      value = billAmt;
+
+      appendTd(tr, value);
+
+      expect(tr.childNodes[0].innerText).toEqual(`${value}`);
     });
   });
 });
