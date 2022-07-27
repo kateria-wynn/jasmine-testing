ab06721764ebae4cb7feb40c955e7adaa29e3dc3
Kateria Wynn <iamkateria@gmail.com>
2022-07-27 10:50:24 -0700

    added funcs for sumPaymentTotal tests, calculateTipPercent tests

--- tip-pool/helpers.test.js
+++ tip-pool/helpers.test.js
@@ -1,8 +1,4 @@
 describe('Helpers tests', function () {
-  allPayments = {
-    payment1: { billAmt: '100', tipAmt: '20', tipPercent: 20 },
-  };
-
   describe('sumPaymentTotal tests', function () {
     it('should accept a tip percentage', function () {
       submitPaymentInfo();
@@ -20,5 +16,14 @@ describe('Helpers tests', function () {
       expect(sumPaymentTotal('tipAmt')).toEqual(20);
     });
   });
-  describe('calculateTipPercent tests', function () {});
+  describe('calculateTipPercent tests', function () {
+    it('should calculate tip percentage correctly', function () {
+      allPayments = {
+        payment1: { billAmt: '100', tipAmt: '20', tipPercent: 20 },
+      };
+      billAmt = allPayments['payment1'].billAmt;
+      tipAmt = allPayments['payment1'].tipAmt;
+      expect(calculateTipPercent(billAmt, tipAmt)).toEqual(20);
+    });
+  });
 });
