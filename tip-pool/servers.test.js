8a73291b82dcf8e84f2eeb421ea06c621faf1468
Kateria Wynn <iamkateria@gmail.com>
2022-07-27 19:30:28 -0700

    added func for updateServerTable tests, afterEach

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -28,13 +28,22 @@ describe('Servers test (with setup and tear-down)', function () {
       // expect server table to not have children
       expect(serverTable.hasChildNodes()).toBe(true);
     });
+    it('should update servertable on updateServerTable()', function () {
+      submitServerInfo();
+      updateServerTable();
+
+      let curTdList = document.querySelectorAll('#serverTable tbody tr td');
+
+      expect(curTdList.length).toEqual(3);
+      expect(curTdList[0].innerText).toEqual('Alice');
+      expect(curTdList[1].innerText).toEqual('$0.00');
+      expect(curTdList[2].innerText).toEqual('X');
+    });
   });
+
   afterEach(function () {
-    for (let server in allServers) {
-      let serverNum = server.slice(-1);
-      let currServer = document.querySelector(`#server${serverNum}`);
-      currServer.parentElement.replaceChildren();
-    }
+    serverId = 0;
+    serverTbody.innerHTML = '';
     allServers = {};
   });
 });
