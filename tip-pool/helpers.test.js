97a06d7d5bd19caa1f052644dcb19cc855ef883f
Kateria Wynn <iamkateria@gmail.com>
2022-07-27 17:43:23 -0700

    updated sumPaymentTotal, calculateTipPercent, appendTd tests

--- tip-pool/helpers.test.js
+++ tip-pool/helpers.test.js
@@ -1,29 +1,44 @@
 describe('Helpers tests', function () {
+  beforeEach(function () {
+    billAmtInput.value = 100;
+    tipAmtInput.value = 20;
+    submitPaymentInfo();
+  });
   describe('sumPaymentTotal tests', function () {
-    it('should accept a tip percentage', function () {
-      submitPaymentInfo();
-
+    it('should sum total tip percent on sumPaymentTotal()', function () {
       expect(sumPaymentTotal('tipPercent')).toEqual(20);
-    });
-    it('should accept a bill amount', function () {
+
+      billAmtInput.value = 100;
+      tipAmtInput.value = 20;
+
       submitPaymentInfo();
 
-      billAmt = allPayments['payment1'].billAmt;
-
+      expect(sumPaymentTotal('tipPercent')).toEqual(40);
+    });
+    it('should sum total bill amount of all payments on sumPaymentTotal()', function () {
       expect(sumPaymentTotal('billAmt')).toEqual(100);
-    });
-    it('should accept a tip amount', function () {
+
+      billAmtInput.value = 100;
+      tipAmtInput.value = 20;
+
       submitPaymentInfo();
 
+      expect(sumPaymentTotal('billAmt')).toEqual(200);
+    });
+    it('should sum total tip amount of all payments on sumPaymentTotal()', function () {
       expect(sumPaymentTotal('tipAmt')).toEqual(20);
+
+      billAmtInput.value = 100;
+      tipAmtInput.value = 20;
+
+      submitPaymentInfo();
+
+      expect(sumPaymentTotal('tipAmt')).toEqual(40);
     });
   });
 
   describe('calculateTipPercent tests', function () {
     it('should calculate tip percentage correctly', function () {
-      submitPaymentInfo();
-      createCurPayment();
-
       billAmt = allPayments['payment1'].billAmt;
       tipAmt = allPayments['payment1'].tipAmt;
 
@@ -34,11 +49,23 @@ describe('Helpers tests', function () {
   describe('appendTd tests', function () {
     it('should upate the HTML with the new bill amount', function () {
       tr = document.createElement('tr');
-      value = billAmt;
+      value = allPayments['payment1'].billAmt;
 
       appendTd(tr, value);
 
+      expect(tr.children.length).toEqual(1);
       expect(tr.childNodes[0].innerText).toEqual(`${value}`);
     });
+  });
+  afterEach(function () {
+    billAmtInput.value = '';
+    tipAmtInput.value = '';
+    paymentTbody.innerHTML = '';
+    summaryTds[0].innerHTML = '';
+    summaryTds[1].innerHTML = '';
+    summaryTds[2].innerHTML = '';
+    serverTbody.innerHTML = '';
+    allPayments = {};
+    paymentId = 0;
   });
 });
